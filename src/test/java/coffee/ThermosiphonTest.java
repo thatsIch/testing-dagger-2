d08e337ffe8b26fceabe6d5e67f1049531f008ca
thatsIch <thatsIch@mail.de>
2016-04-06 23:29:32 +0200

    Add successful constructor test

--- src/test/java/coffee/ThermosiphonTest.java
+++ src/test/java/coffee/ThermosiphonTest.java
@@ -3,6 +3,7 @@ package coffee;
 
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 
 public class ThermosiphonTest
@@ -13,5 +14,15 @@ public class ThermosiphonTest
 		Assertions.assertThatThrownBy( () -> new Thermosiphon( null ) )
 			.as( "The constructor should not be able to be initialized with 'null'." )
 			.isExactlyInstanceOf( NullPointerException.class );
+	}
+
+	@Test
+	public void test_constructingThermoshiphon_WithHeater_ShouldWork() throws Exception
+	{
+		final Heater heaterMock = Mockito.mock( Heater.class );
+
+		Assertions.assertThat( new Thermosiphon( heaterMock ) )
+			.isEqualToComparingFieldByFieldRecursively( new Thermosiphon( heaterMock ) )
+			.as( "Both thermosiphons use the same heater and should be the same and not 'null'." );
 	}
 }
