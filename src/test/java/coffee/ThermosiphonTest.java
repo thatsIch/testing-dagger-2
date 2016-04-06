9db526670464217bb798d967fe8a83a01113a0ea
thatsIch <thatsIch@mail.de>
2016-04-06 23:56:38 +0200

    Added tests for pumping

--- src/test/java/coffee/ThermosiphonTest.java
+++ src/test/java/coffee/ThermosiphonTest.java
@@ -1,6 +1,9 @@
 package coffee;
 
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -13,7 +16,8 @@ public class ThermosiphonTest
 	{
 		Assertions.assertThatThrownBy( () -> new Thermosiphon( null ) )
 			.as( "The constructor should not be able to be initialized with 'null'." )
-			.isExactlyInstanceOf( NullPointerException.class );
+			.isExactlyInstanceOf( NullPointerException.class )
+			.hasNoCause();
 	}
 
 	@Test
@@ -24,5 +28,51 @@ public class ThermosiphonTest
 		Assertions.assertThat( new Thermosiphon( heaterMock ) )
 			.isEqualToComparingFieldByFieldRecursively( new Thermosiphon( heaterMock ) )
 			.as( "Both thermosiphons use the same heater and should be the same and not 'null'." );
+	}
+
+	@Test
+	public void test_pump_WithColdHeater_ShouldWork() throws Exception
+	{
+		// given
+		final Heater heaterMock = Mockito.mock( Heater.class );
+		Mockito.when( heaterMock.isHot() ).thenReturn( false );
+
+		// redirect  sysout
+		final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setOut( new PrintStream( outContent ) );
+
+		// when
+		new Thermosiphon( heaterMock ).pump();
+
+		// cleanup
+		System.setOut(null);
+
+		// then
+		Assertions.assertThat( outContent.toString() )
+			.isEqualTo( "" )
+			.as( "Since the heater is cold, the thermosiphon is not working and should not print anything into system.out." );
+	}
+
+	@Test
+	public void test_pump_WithHotHeater_ShouldWork() throws Exception
+	{
+		// given
+		final Heater heaterMock = Mockito.mock( Heater.class );
+		Mockito.when( heaterMock.isHot() ).thenReturn( true );
+
+		// redirect  sysout
+		final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		System.setOut( new PrintStream( outContent ) );
+
+		// when
+		new Thermosiphon( heaterMock ).pump();
+
+		// cleanup
+		System.setOut(null);
+
+		// then
+		Assertions.assertThat( outContent.toString() )
+			.isEqualTo( "=> => pumping => =>" + System.lineSeparator() )
+			.as( "Since the heater is hot, the thermosiphon should work and should print a String into the system.out." );
 	}
 }
